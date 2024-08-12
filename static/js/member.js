5037a258bcbb9baaf0ae62c9589e2ca9c8ad94c7
Alfyn <qq10171028@gamil.com>
2024-08-12 19:55:38 +0800

    finished get use pocket restaurants with paging function

--- static/js/member.js
+++ static/js/member.js
@@ -1,8 +1,33 @@
+let pocket_next_page = 0;
+
 // 回到首頁
 document.querySelector(".title").addEventListener("click", (e) => {
   location.href = "/";
 });
 // 登出
+document.querySelector(".logout").addEventListener("click", (e) => {
+  localStorage.removeItem("user_token");
+  location.href = "/";
+});
+
+// 獲得會員口袋清單並渲染
+async function getMyPocket(page) {
+  let url = "/api/pocket?page=" + page;
+  // 渲染口袋
+  const response = await fetch(url, {
+    method: "GET",
+    headers: {
+      Authorization: "Bearer " + localStorage.getItem("user_token"),
+    },
+  });
+
+  const data = await response.json();
+  // console.log(data);
+  for (let i = 0; i < data.data.length; i++) {
+    renderPocket(data.data[i]);
+  }
+  pocket_next_page = data.next_page;
+}
 
 // 初始化
 async function init() {
@@ -35,21 +60,10 @@ async function init() {
       console.error("沒抓到/api/user/auth的資料", error);
     }
   }
-  // 渲染口袋
-  console.log("test");
-  const response = await fetch("/api/pocket?page=0", {
-    method: "GET",
-    headers: {
-      Authorization: "Bearer " + localStorage.getItem("user_token"),
-    },
-  });
-
-  const data = await response.json();
-
-  console.log(data);
+  getMyPocket(0);
 }
-init();
 
+// 渲染會員資訊
 function renderProfile(name, photoURL, avg_rating, pocketCount, commentCount) {
   document.querySelector(".user-name").innerHTML = name;
   document.querySelector(".user-photo-url").src =
@@ -60,4 +74,54 @@ function renderProfile(name, photoURL, avg_rating, pocketCount, commentCount) {
   document.querySelector(".user-checkIn-count").innerHTML = commentCount;
 }
 
-const restaurantBox = function renderPocket(data) {};
+// 渲染口袋清單
+function renderPocket(data) {
+  let open_tag;
+  if (data.open === true) {
+    open_tag = '<div class="restaurant-open">營業中</div>';
+  } else if (data.open === false) {
+    open_tag = '<div class="restaurant-close">休息中</div>';
+  } else {
+    open_tag = "";
+  }
+  const restaurantBox = `
+    <div class="restaurant-box" id=${data.id}>
+      <img
+        src="${data.img}"
+        alt="餐廳圖片"
+        class="restaurant-img"
+      />
+      <div class="restaurant-info">
+        <div class="restaurant-name">${data.name}</div>
+        <img
+          src="/static/image/trash-solid.svg"
+          alt="垃圾桶"
+          class="delete-btn"
+        />
+      </div>
+      ${open_tag}
+    </div>`;
+  document
+    .querySelector(".restaurant-box-group")
+    .insertAdjacentHTML("beforeend", restaurantBox);
+}
+
+init();
+
+// 滾動口袋清單
+const mrtContainer = document.querySelector(".restaurant-box-group");
+const rightBtn = document.querySelector(".pocket-right-arrow");
+const leftBtn = document.querySelector(".pocket-left-arrow");
+rightBtn.addEventListener("click", (e) => {
+  mrtContainer.scrollLeft += mrtContainer.clientWidth * 0.75;
+  if (
+    mrtContainer.scrollLeft + mrtContainer.clientWidth >=
+      mrtContainer.scrollWidth * 0.8 &&
+    pocket_next_page != null
+  ) {
+    getMyPocket(pocket_next_page);
+  }
+});
+leftBtn.addEventListener("click", (e) => {
+  mrtContainer.scrollLeft -= mrtContainer.clientWidth * 0.75;
+});
