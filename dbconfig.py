3f46069b752bb332165eef0d047808683b8ba383
Alfyn <qq10171028@gamil.com>
2024-08-23 00:02:49 +0800

    update docker-compose file with ec2 setting

--- dbconfig.py
+++ dbconfig.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_HOST = os.getenv('DATABASE_HOST')
+# DATABASE_HOST = "localhost"
 DATABASE_USER = os.getenv('DATABASE_USER')
 DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
 DATABASE_NAME = os.getenv('DATABASE_NAME')
