b1a02c9ff2a62eda633ef94abc431308a6f66f6a
Alfyn <qq10171028@gamil.com>
2024-09-07 14:37:02 +0800

    optimize connetion setting which ensure conntion will be release

--- dbconfig.py
+++ dbconfig.py
@@ -14,69 +14,111 @@ DATABASE_NAME = os.getenv('DATABASE_NAME')
 
 class Database:
     mydb = {
-        "host" : DATABASE_HOST,
-        "user" : DATABASE_USER,
-        "password" : DATABASE_PASSWORD,
-        "database" : DATABASE_NAME
+        "host": DATABASE_HOST,
+        "user": DATABASE_USER,
+        "password": DATABASE_PASSWORD,
+        "database": DATABASE_NAME
     }
     connection_pool = mysql.connector.pooling.MySQLConnectionPool(pool_name="my_pool", pool_size=32, **mydb)
-    
-    def read_all(sql, val=None):
-        connect = Database.connection_pool.get_connection()
-        mycursor = connect.cursor(dictionary=True)
-        mycursor.execute("SET time_zone = 'Asia/Taipei';")
-        mycursor.execute(sql, val)
-        result = mycursor.fetchall()
-        mycursor.close()
-        connect.close()
-        return result
-    
-    def read_one(sql, val=None):
-        connect = Database.connection_pool.get_connection()
-        mycursor = connect.cursor(dictionary=True)
-        mycursor.execute("SET time_zone = 'Asia/Taipei';")
-        mycursor.execute(sql, val)
-        result = mycursor.fetchone()
-        mycursor.close()
-        connect.close()
-        return result
 
+    @staticmethod
+    def read_all(sql, val=None):
+        connect = None
+        mycursor = None
+        try:
+            connect = Database.connection_pool.get_connection()
+            mycursor = connect.cursor(dictionary=True)
+            mycursor.execute("SET time_zone = 'Asia/Taipei';")
+            mycursor.execute(sql, val)
+            result = mycursor.fetchall()
+            return result
+        finally:
+            if mycursor:
+                mycursor.close()
+            if connect:
+                connect.close()
+
+    @staticmethod
+    def read_one(sql, val=None):
+        connect = None
+        mycursor = None
+        try:
+            connect = Database.connection_pool.get_connection()
+            mycursor = connect.cursor(dictionary=True)
+            mycursor.execute("SET time_zone = 'Asia/Taipei';")
+            mycursor.execute(sql, val)
+            result = mycursor.fetchone()
+            return result
+        finally:
+            if mycursor:
+                mycursor.close()
+            if connect:
+                connect.close()
+
+    @staticmethod
     def create(sql, val=None):
-        connect = Database.connection_pool.get_connection()
-        mycursor = connect.cursor(dictionary=True)
-        mycursor.execute(sql, val)
-        connect.commit()
-        affected_rows = mycursor.rowcount
-        mycursor.close()
-        connect.close()
-        return affected_rows
-    
+        connect = None
+        mycursor = None
+        try:
+            connect = Database.connection_pool.get_connection()
+            mycursor = connect.cursor(dictionary=True)
+            mycursor.execute(sql, val)
+            connect.commit()
+            affected_rows = mycursor.rowcount
+            return affected_rows
+        finally:
+            if mycursor:
+                mycursor.close()
+            if connect:
+                connect.close()
+
+    @staticmethod
     def create_and_return_id(sql, val=None):
-        connect = Database.connection_pool.get_connection()
-        mycursor = connect.cursor(dictionary=True)
-        mycursor.execute(sql, val)
-        last_id = mycursor.lastrowid
-        connect.commit()
-        mycursor.close()
-        connect.close()
-        return last_id
-    
+        connect = None
+        mycursor = None
+        try:
+            connect = Database.connection_pool.get_connection()
+            mycursor = connect.cursor(dictionary=True)
+            mycursor.execute(sql, val)
+            last_id = mycursor.lastrowid
+            connect.commit()
+            return last_id
+        finally:
+            if mycursor:
+                mycursor.close()
+            if connect:
+                connect.close()
+
+    @staticmethod
     def update(sql, val=None):
-        connect = Database.connection_pool.get_connection()
-        mycursor = connect.cursor(dictionary=True)
-        mycursor.execute(sql, val)
-        connect.commit()
-        affected_rows = mycursor.rowcount
-        mycursor.close()
-        connect.close()
-        return affected_rows
-    
+        connect = None
+        mycursor = None
+        try:
+            connect = Database.connection_pool.get_connection()
+            mycursor = connect.cursor(dictionary=True)
+            mycursor.execute(sql, val)
+            connect.commit()
+            affected_rows = mycursor.rowcount
+            return affected_rows
+        finally:
+            if mycursor:
+                mycursor.close()
+            if connect:
+                connect.close()
+
+    @staticmethod
     def delete(sql, val=None):
-        connect = Database.connection_pool.get_connection()
-        mycursor = connect.cursor(dictionary=True)
-        mycursor.execute(sql, val)
-        connect.commit()
-        affected_rows = mycursor.rowcount
-        mycursor.close()
-        connect.close()
-        return affected_rows
+        connect = None
+        mycursor = None
+        try:
+            connect = Database.connection_pool.get_connection()
+            mycursor = connect.cursor(dictionary=True)
+            mycursor.execute(sql, val)
+            connect.commit()
+            affected_rows = mycursor.rowcount
+            return affected_rows
+        finally:
+            if mycursor:
+                mycursor.close()
+            if connect:
+                connect.close()
